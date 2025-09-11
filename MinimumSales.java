7d2b1f5e2f9e2c53595d54ef06015f2f7bfc03e5
sarankumar143007 <sarankumar143t@gmail.com>
2025-09-11 16:23:12 +0530

    Add files via upload

---
+++ MinimumSales.java
@@ -0,0 +1,16 @@
+package sales;
+
+public class MinimumSales {
+    public static void main(String[] args) {
+        int[] sales = {120, 340, 560, 230, 999};
+        int min_sale = Integer.MAX_VALUE; 
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < min_sale) {
+                min_sale = sales[i];  
+            }
+        }
+
+        System.out.println("Min sale = " + min_sale);
+    }
+}
