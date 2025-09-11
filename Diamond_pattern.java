fa54b48e80d8f5db781650c18c776ad128d762d2
sarankumar143007 <sarankumar143t@gmail.com>
2025-09-11 15:48:13 +0530

    Add files via upload

---
+++ Diamond_pattern.java
@@ -0,0 +1,31 @@
+package diamond;
+
+import java.util.Scanner; 
+public class Diamond_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows (half diamond): ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
