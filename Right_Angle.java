cedfc8c3b2647d8c3b9ec56e9574fbcc8b5717c3
sarankumar143007 <sarankumar143t@gmail.com>
2025-09-11 16:25:22 +0530

    Add files via upload

---
+++ Right_Angle.java
@@ -0,0 +1,21 @@
+package right_triangle;
+
+import java.util.Scanner;
+
+public class Right_Angle {
+    public static void main(String[] args) {
+        int n, i, j;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        n = sc.nextInt();
+
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("*"); 
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
