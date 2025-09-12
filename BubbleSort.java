fd672c580b3c0bff8c8bce8c2bbfc93d05a1f0fe
sarankumar143007 <sarankumar143t@gmail.com>
2025-09-12 12:30:28 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,30 @@
+package Sort;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            // Last i elements are already in place
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {64, 25, 12, 22, 11};
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
