492777c5eb9c0d401f4d04041a309cbbc89bfca0
sarankumar143007 <sarankumar143t@gmail.com>
2025-09-25 10:15:56 +0530

    Add files via upload

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,18 +1,18 @@
 package Series;
 
 public class FibonacciSeries {
-	public static void main(String[] args) {
-        int f1 = 0, f2 = 1, f3;
-        int n = 5; 
+	static int fib(int n) {
+        if (n <= 1) 
+            return n;
+        return fib(n - 1) + fib(n - 2);  
+    }
 
-        System.out.print(f1 + " " + f2); 
-
-        for (int i = 1; i <= n - 2; i++) {
-            f3 = f1 + f2;
-            System.out.print(" " + f3);
-            f1 = f2;
-            f2 = f3;
+    public static void main(String args[]) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
         }
     }
 }
+
 
